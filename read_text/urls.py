37a403306cf6bc5e5303e215f01ab4e063f8c4cd
forlesendev <forlesen.app@gmail.com>
2017-02-21 20:36:18 +0000

    Text input

--- read_text/urls.py
+++ read_text/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
             url(r'^$', views.display, name='display'),
+            url(r'^text_input/$', views.text_input, name='text_input'),
             ]
--- read_text/views.py
+++ read_text/views.py
@@ -12,9 +12,14 @@ def display(request):
     'wissen','sehen','lassen','stehen','finden','bleiben','liegen',\
     'heissen','denken','nehmen','tun','duerfen','glauben','halten',\
     'nennen','moegen','zeigen','fuehren','sprechen','sprechen',\
-    'bringen','leben','fahren','meinen','meinen','fragen','kennen',\
-    'gelten','stellen','spielen','arbeiten','brauchen','folgen','lernen']
+    'bringen','leben','fahren','meinen','fragen','kennen',\
+    'gelten','stellen','spielen','arbeiten','brauchen','folgen',\
+    'lernen','bestehen','verstehen','setzen','bekommen','beginnen']
     for wrd_word_i in range(len(wrd_word)):
-        wrd = Word(word=wrd_word[wrd_word_i])
-        wrd.save()
+        if not Word.objects.filter(word=wrd_word[wrd_word_i]).exists():
+            wrd = Word(word=wrd_word[wrd_word_i])
+            wrd.save()
     return render(request,'template.tmpl',{'obj':Word.objects.all()})
+
+def text_input(request):
+    return render(request,'text_input.html')
